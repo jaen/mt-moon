5077fb6a0c40aadf40d4d79d3ae458f11accd359
Tomek Mańko <jaennirin@gmail.com>
2015-11-02 12:59:09 +0100

    IT'S ALIVE

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    ifstream plot_file;
+
+    plot_file.open("plot.txt", ifstream::in);
+
     cout << "Hello Kanto!" << endl;
+
+    while(plot_file.good()) {
+        string line;
+
+        getline(plot_file, line);
+
+        cout << line << endl;
+    }
+
+    plot_file.close();
+
     return 0;
 }
